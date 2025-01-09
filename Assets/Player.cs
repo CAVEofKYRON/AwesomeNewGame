520542c84625064ea13fca1b67c82c0926d891a4
KYRONsCAVE <tzyntares@gmail.com>
2025-01-09 08:10:20 +0100

    feat: movement x and y

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,7 +1,14 @@
 using UnityEngine;
 
 public class Player : MonoBehaviour
+
 {
+    public Rigidbody2D rb;
+
+    public float moveSpeed;
+    private float xInput;
+    public float jumpForce;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,10 +18,13 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump"))
-        {
-            Debug.Log("Jump");
-        }
+        xInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocity.y);
+        
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
     }
 }
